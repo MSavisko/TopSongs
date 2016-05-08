a2ced7caa5b318952120668b4084e2b371f477ca
Maksym Savisko <stowyn@gmail.com>
2016-05-08 11:55:42 +0300

    in progress

--- Top Songs/ViewController/MSNewsTVC.m
+++ Top Songs/ViewController/MSNewsTVC.m
@@ -8,6 +8,9 @@
 
 #import "MSNewsTVC.h"
 
+#import "XMLDictionary.h"
+#import "AFNetworking.h"
+
 @interface MSNewsTVC ()
 @end
 
@@ -21,7 +24,21 @@
 
 #pragma mark - Helper Methods
 - (void) loadSongsList {
+    NSURL *url = [NSURL URLWithString:@"http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=25/xml"];
     
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFXMLParserResponseSerializer serializer];
+    
+    manager.responseSerializer.acceptableContentTypes =  [manager.responseSerializer.acceptableContentTypes setByAddingObject:@"application/atom+xml"];
+    
+    [manager GET:url.absoluteString parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        //Success
+        NSDictionary *d0 = [NSDictionary dictionaryWithXMLParser:(NSXMLParser*)responseObject];
+        NSLog(@"Resonse object: %@", (NSXMLParser*)responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        //Fail
+        NSLog(@"Error: %@", error);
+    }];
 }
 
 @end
