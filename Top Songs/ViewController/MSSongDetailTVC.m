dbdf8826701257ce4a72772f6c8ec185540449d3
Maksym Savisko <stowyn@gmail.com>
2016-05-09 16:28:40 +0300

    Init share with Twitter

--- Top Songs/ViewController/MSSongDetailTVC.m
+++ Top Songs/ViewController/MSSongDetailTVC.m
@@ -8,10 +8,12 @@
 
 #import "MSSongDetailTVC.h"
 #import "AFNetworking.h"
+#import <Social/Social.h>
 
-@interface MSSongDetailTVC ()
+@interface MSSongDetailTVC () <UIActionSheetDelegate, UIAlertViewDelegate>
 @property (strong, nonatomic) NSArray *songItems;
 @property (weak, nonatomic) IBOutlet UIImageView *songImageView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *shareButton;
 
 @end
 
@@ -23,6 +25,16 @@
     self.title = self.song.artist;
     self.songItems = @[@"title", @"collectionName", @"releaseDate", @"price", @"url"];
     [NSThread detachNewThreadSelector:@selector(downloadAndLoadImage) toTarget:self withObject:nil];
+}
+
+#pragma makr - Action
+- (IBAction)shareButtonPressed:(id)sender {
+    UIActionSheet *shareActionSheet = [[UIActionSheet alloc]initWithTitle:@"How to share?"
+                                                                 delegate:self
+                                                        cancelButtonTitle:@"Cancel"
+                                                   destructiveButtonTitle:nil
+                                                        otherButtonTitles:@"Twitter", @"Facebook", @"E-mail", nil];
+    [shareActionSheet showInView:self.view];
 }
 
 #pragma mark - TableViewDelegate
@@ -70,6 +82,70 @@
     return cell;
 }
 
+#pragma mark - UIActionSheetDelegate
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) { //Twitter
+        if ([self stringResultTwitter].length > 116) {
+            [self showAlertWithMessage:@"The result is too long to send the tweet. You will need to manually cut it!" tag:100];
+        } else {
+            [self shareWithTwitter];
+        }
+    }
+}
+
+#pragma mark - UIAlertViewDelegate
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if (alertView.tag == 100) {
+        [self shareWithTwitter];
+    }
+    if (alertView.tag == 200) {
+        [self shareWithFacebook];
+    }
+}
+
+#pragma mark - ShareMethods
+- (void) shareWithTwitter {
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
+    {
+        SLComposeViewController *tweet = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+        [tweet setInitialText:[self stringResultTwitter]];
+        NSData *data = [NSData dataWithContentsOfURL:self.song.imagePath];
+        UIImage *image = [[UIImage alloc]initWithData:data];
+        [tweet addImage:image];
+        [tweet setCompletionHandler:^(SLComposeViewControllerResult result)
+         {
+             if (result == SLComposeViewControllerResultCancelled)
+             {
+                 //The user cancelled
+             }
+             else if (result == SLComposeViewControllerResultDone)
+             {
+                 //The user sent the tweet
+             }
+         }];
+        [self presentViewController:tweet animated:YES completion:nil];
+    }
+    else
+    {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Twitter"
+                                                        message:@"Twitter integration is not available. A Twitter account must be set up on your device."
+                                                       delegate:self
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+- (void) shareWithFacebook {
+    
+}
+
+#pragma mark - PresentationDataMethod
+- (NSString*) stringResultTwitter {
+    NSString *result = [NSString stringWithFormat:@"Artist: %@\nTrack name: %@\nCollection name: %@\nReleased: %@\nPrice: %@", self.song.artist, self.song.name, self.song.collectionName, self.song.releaseDate, self.song.price];
+    return result;
+}
+
 #pragma mark - HelperMethods
 - (void) downloadAndLoadImage {
     NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
@@ -99,7 +175,16 @@
                                                                 
                                                             }];
     [downloadTask resume];
-    
+}
+
+- (void) showAlertWithMessage:(NSString*)message tag:(NSInteger)tag {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Warning!"
+                                                    message:message
+                                                   delegate:self
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    alert.tag = tag;
+    [alert show];
 }
 
 @end
